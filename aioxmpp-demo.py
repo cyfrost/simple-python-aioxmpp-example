62e22ad4c3d81c15255fec948c80dd2ca654a01e
Cyrus Frost <cyrus.frost@hotmail.com>
2020-01-21 23:39:56 +0530

    init repo with example

---
+++ aioxmpp-demo.py
@@ -0,0 +1,24 @@
+import asyncio
+import aioxmpp
+import configparser
+from framework import Example, exec_example
+
+class SendMessage(Example):
+    @asyncio.coroutine
+    def run_simple_example(self):
+        recipient = self.config.get("global", "xmpp_slave_jid")
+        msgData = self.config.get("global", "xmpp_command")
+
+        msg = aioxmpp.stanza.Message(
+            to=aioxmpp.JID.fromstr(recipient),
+            type_=aioxmpp.MessageType.CHAT,
+        )
+
+        msg.body[None] = msgData
+
+        print("Sending message...")
+        yield from self.client.send(msg)
+        print("Message sent!")
+
+if __name__ == "__main__":
+    exec_example(SendMessage())
